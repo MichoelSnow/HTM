59550256f768fcc301e013f256d7ab6d9988e12e
mseinstein <protect3f@gmail.com>
2017-07-06 12:55:33 -0400

    started running tests

---
+++ Tests/Appts/appt_htm_prep.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 06 10:38:29 2017
+
+@author: msnow1
+"""
+
+import pandas as pd
+from matplotlib.dates import date2num, DateFormatter
+import numpy as np
+import datetime as dt
+
+
+def HTMprep(DataFl,timeNm,VarNm):
+    line = pd.DataFrame({timeNm:["datetime"," "],VarNm:["float"," "]},index=[0,1])
+    return pd.concat([line, DataFl],join = 'inner',ignore_index =True)
+
+def HTMprep2(DataFl):
+    cols = DataFl.columns
+    var_dict = {cols[0]:["datetime"," "]}
+    for i in cols[1:]:
+        var_dict[i] = ["float"," "]
+    line = pd.DataFrame(var_dict,index=[0,1])
+#    line = pd.DataFrame({timeNm:["datetime"," "],VarNm:["float"," "],VarNm2:["float"," "]},index=[0,1])
+    return pd.concat([line, DataFl],join = 'inner',ignore_index =True)
+
+# %%
+
+appt = pd.read_csv('E:\\MyDocuments\\HospAgg\\Appts\\appt_D_2007_2015.csv',
+                   na_values="")
+appt.APPOINTMENTDATETIME = pd.to_datetime(appt.APPOINTMENTDATETIME, format = "%Y-%m-%d %H:%M:%S", errors = 'coerce')
+
+steps = 3
+appt.rename(inplace = True, columns = {"APPOINTMENTDATETIME":"timestamp","SHOW":"Ct"})
+cols = appt.columns
+var_dict = dict()
+for i in cols[2:]:
+    appt[i] = appt[i].shift(-1*steps,axis=0)
+appt = appt.fillna(0)    
+appt_htm = HTMprep2(appt)
+
+
+appt_htm.to_csv("E:\\MyDocuments\\GitHub\\HTM\\Tests\\Appts\\appt_htm_%ssteps.csv" % (steps),index=False)
+
+
---
+++ Tests/Appts/appt_model_comp.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 06 12:38:18 2017
+
+@author: msnow1
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 29 10:05:16 2017
+
+@author: msnow1
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+from matplotlib.dates import date2num, DateFormatter
+from statsmodels.tsa import ar_model, arima_model
+
+
+def MSE(Vals,Pred,WndwDys,DyStps):
+    Resid = (abs(Pred - Vals))**2/(WndwDys*DyStps)
+    return Resid.rolling(WndwDys*DyStps,center=False).sum()
+
+# %% HTM Prediction
+
+WndwDys = 30
+DyStps = 1
+PredAhead = 1
+
+InputName = 'E:\\MyDocuments\\GitHub\\HTM\\Tests\\appts\\appt_htm_1steps_out.csv'
+appt_htm = pd.read_csv(InputName, na_values="",index_col=0,usecols=[0,1,2])
+appt_htm.index = pd.to_datetime(appt_htm.index, format = "%Y-%m-%d %H:%M:%S", errors = 'coerce') 
+appt_htm['MSE'] = MSE(appt_htm.Ct,appt_htm.prediction,WndwDys,DyStps)
+
+#ed_3h = pd.DataFrame(ed_3h_htm.prediction)
+appt = appt_htm[['Ct','prediction']]
+appt.rename(columns = {"prediction":"HTM"},inplace=True)
+appt_mse = pd.DataFrame(appt_htm.MSE)
+appt_mse.rename(columns = {"MSE":"HTM"},inplace=True)
+
+
+# %% Mean Ct over past 30 days per time step
+appt_mn = appt_htm[['Ct']]
+appt_mn['prediction'] = appt_mn.Ct.rolling(WndwDys,center=False).mean()
+appt_mn.prediction = appt_mn.prediction.shift(PredAhead*DyStps)
+appt_mn['MSE'] = MSE(appt_mn.Ct,appt_mn.prediction,WndwDys,DyStps)
+
+appt['RollMn'] = appt_mn.prediction
+appt_mse['RollMn'] = appt_mn.MSE
+
+# %% AR Model
+appt_ar = appt_htm[['Ct']]
+ar_mdl = ar_model.AR(appt_ar)
+ar_fit = ar_mdl.fit(maxlag=(WndwDys*DyStps))
+appt_ar['prediction'] = ar_fit.predict()
+appt_ar['MSE'] = MSE(appt_ar.Ct,appt_ar.prediction,WndwDys,DyStps)
+
+
+appt['AR'] = appt_ar.prediction
+appt_mse['AR'] = appt_ar.MSE
+
+# %% ARMA model
+
+
+ed_3h_arima = ed_3h_htm[['Ct']]
+arima_mdl = arima_model.ARIMA(ed_3h_arima,(30,1,30))
+arima_fit = arima_mdl.fit()
+ed_3h_arima['prediction'] = ed_3h_arima.predict()
+ed_3h_arima['MSE'] = MSE(ed_3h_arima.Ct,ed_3h_arima.prediction,WndwDys,DyStps)
+
+
+ed_3h['ARIMA'] = ed_3h_arima.prediction
+ed_3h_mse['ARIMA'] = ed_3h_arima.MSE
+
+
+
+
+
+# %% Plot
+appt['dates'] = [date2num(date) for date in appt.index]
+appt_mse['dates'] = [date2num(date) for date in appt_mse.index]
+gs = gridspec.GridSpec(2,1, height_ratios=[3, 1]) 
+ax0 = plt.subplot(gs[0])
+ax1 = plt.subplot(gs[1], sharex=ax0)
+ax0.set_prop_cycle('color',['b', 'orange','g','r'])
+ax1.set_prop_cycle('color',['orange','g','r'])
+MainGraph = appt.plot(x = 'dates',y = ['Ct','RollMn','HTM','AR'], ax = ax0)
+AnomalyGraph = appt_mse.plot(x = 'dates', y=['RollMn','HTM','AR'], ax=ax1)
+dateFormatter = DateFormatter('%m/%d/%y')
+MainGraph.xaxis.set_major_formatter(dateFormatter)    
+AnomalyGraph.xaxis.set_major_formatter(dateFormatter)
+ax1.set_xlabel('Dates')
+ax1.set_ylabel('MSE')
+ax0.set_ylabel('Traige Cts')
+
+
+
+
+
+
+
+
+
+
